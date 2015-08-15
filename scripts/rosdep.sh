538ba76785e63438a0b2dd04047119d39e99a32f
Arnaud Ramey <arnaud.a.ramey@gmail.com>
2015-08-15 14:23:49 +0000

    prepairing migration

--- scripts/rosdep.sh
+++ scripts/rosdep.sh
@@ -5,30 +5,20 @@
 # not compile 'binutils-gold' beacuse cause broken ld program fail
 
 PACKAGES="ros-`rosversion -d`-openni-camera
-          ros-`rosversion -d`-openni-tracker
           ros-`rosversion -d`-openni-launch
-          libgmp-dev
           ros-`rosversion -d`-resource-retriever
-          freeglut3
-          freeglut3-dev
           libgstreamer0.10-dev
           libv4l-dev"
 
 sudo apt-get install $PACKAGES
+exit
 
 # ar_pose
-if [ -z $ROS_WORKSPACE ]; then
-    echo ""
-    echo "###########################################################"
-    echo 'ERROR. $ROS_WORKSPACE missing, check .bashrc file. Exiting.'
-    echo "###########################################################"
-    echo ""
-else
-
-
-    cd $ROS_WORKSPACE
-    git clone https://github.com/ar-tools/ar_tools.git src/
-    catkin_make --only-pkg-with-deps artoolkit
-    catkin_make -DCATKIN_WHITELIST_PACKAGES=""
-
-fi
+source /opt/ros/indigo/setup.bash
+source /home/arnaud/catkin_ws/devel/setup.bash
+roscd
+cd ../src/
+git clone https://github.com/ar-tools/ar_tools.git
+cd ..
+catkin_make --only-pkg-with-deps artoolkit
+#~ catkin_make -DCATKIN_WHITELIST_PACKAGES=""
